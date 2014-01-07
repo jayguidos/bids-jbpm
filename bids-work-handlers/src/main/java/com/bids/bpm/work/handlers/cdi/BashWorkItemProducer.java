89e867d6d914ba5b04f4672f24c1566cc07ded34
Jay Guidos <jay.guidos@bidstrading.com>
2014-01-07 01:23:33 -0500

    [REPO-ADMIN] project files

---
+++ bids-work-handlers/src/main/java/com/bids/bpm/work/handlers/cdi/BashWorkItemProducer.java
@@ -0,0 +1,14 @@
+/**
+ * ===========================================================================
+ *    Copyright 2012 BIDS Holdings L.P. All rights reserved.
+ * ---------------------------------------------------------------------------
+ * Created on 1/6/14
+ * By bidsjagu
+ *
+ */
+
+package com.bids.bpm.work.handlers.cdi;
+
+public class BashWorkItemProducer
+{
+}
